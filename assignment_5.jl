21be5451345ea8f198a04853007c7397d850fe1d
Jordan Powers <jordanpowers1227@gmail.com>
2020-06-01 16:58:13 -0700

    finished interp appc case

--- assignment_5.jl
+++ assignment_5.jl
@@ -16,11 +16,13 @@ struct IfC
     IfC(test, then, els) = new(test, then, els)
 end
 
+# me
 struct AppC
-    body
+    funexpr
     args::Array{Any}
     AppC(body, args) = new(body, args)
 end
+# end me
 
 struct LamC
     body
@@ -94,6 +96,73 @@ Env("false", BoolV(false),
 nothing))))))))
 
 searlize(v :: Value) =
-if  isa(v, NumV)
+if isa(v, NumV)
     "$(v.val)"
+end
+
+function extend_env(args :: Array{String}, vals :: Array{Value}, env :: Environment)
+    if length(args) != length(vals)
+        error("AQSE: Mismatched arity!")
+    end
+    for i = 1:length(args)
+        env = Environment(args[i], vals[i], env)
+    end
+    return env
+end
+
+function check_numeric_binop_valid_args(args :: Array{Value})
+    if length(args) != 2
+        error("AQSE: bad syntax: primop invalid arguments")
+    end
+    arg1 = args[1]
+    arg2 = args[2]
+    if !isa(arg1, NumV) || !isa(arg2, NumV)
+        error("AQSE: bad syntax: primop invalid arguments")
+    end
+end
+
+function interp_primop(op :: String, args :: Array{Value})
+    if op == "+"
+        check_numeric_binop_valid_args(args)
+        return args[0].val + args[1].val
+    elseif op == "-"
+        check_numeric_binop_valid_args(args)
+        return args[0].val - args[1].val
+    elseif op == "*"
+        check_numeric_binop_valid_args(args)
+        return args[0].val * args[1].val
+    elseif op == "/"
+        check_numeric_binop_valid_args(args)
+        return args[0].val / args[1].val
+    elseif op == "<="
+        check_numeric_binop_valid_args(args)
+        return args[0].val <= args[1].val
+    elseif op == "equal?"
+        if length(args) != 2
+            error("AQSE: bad syntax: equal? invalid arguments")
+        end
+        return args[0] == args[1]
+    elseif op == "error"
+        if length(args < 1) || !isa(args[1], String)
+            error("AQSE: bad syntax: error invalid arguments")
+        end
+        error("AQSE: error: $(args[1].val)")
+    else
+        error("AQSE: invalid primop")
+    end
+end
+
+function interp(expr :: ExprC, env :: Environment) :: Value
+    if isa(expr, AppC)
+        clos :: Value = interp(expr.funexpr, env)
+        argvals :: Array{Value} = map(arg -> interp(arg, env), expr.args)
+        if isa(clos, ClosV)
+            new_env = extend_env(clos.args, argvals, env)
+            return interp(clos.body, new_env)
+        elseif isa(clos, PrimV)
+            interp_primop(clos.op, env)
+        else
+            error("AQSE: Not a function or primop")
+        end
+    end
 end
