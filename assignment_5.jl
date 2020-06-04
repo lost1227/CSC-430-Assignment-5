84b47c2f17cd61e58b66c45f074b1ff9ceb9ad54
Jordan Powers <jordanpowers1227@gmail.com>
2020-06-04 14:33:49 -0700

    Cleanup

--- assignment_5.jl
+++ assignment_5.jl
@@ -3,38 +3,30 @@ using Test
 # ExprC types
 struct NumC
     num::Number
-    NumC(num) = new(num)
 end
 
 struct StrC
     str::String
-    StrC(str) = new(str)
 end
 
 struct IfC
     test
     then
     els
-    IfC(test, then, els) = new(test, then, els)
 end
 
-# me
 struct AppC
     funexpr
     args::Array{Any}
-    AppC(body, args) = new(body, args)
 end
-# end me
 
 struct LamC
     body
     args::Array{String}
-    LamC(body, args) = new(body, args)
 end
 
 struct IdC
     id::String
-    IdC(id) = new(id)
 end
 
 ExprC = Union{NumC, StrC, IfC, AppC, LamC, IdC}
@@ -42,29 +34,24 @@ ExprC = Union{NumC, StrC, IfC, AppC, LamC, IdC}
 # Value Types
 struct NumV
     val :: Real
-    NumV(val) = new(val)
 end
 
 struct BoolV
     val :: Bool
-    BoolV(val) = new(val)
 end
 
 struct StrV
     val :: String
-    StrV(val) = new(val)
 end
 
 struct ClosV
     args :: Array{String}
     body :: ExprC
     env
-    ClosV(args, body, env) = new(args, body, env)
 end
 
 struct PrimV
     op :: String
-    PrimV(op) = new(op)
 end
 
 Value = Union{NumV, BoolV, StrV, ClosV, PrimV}
